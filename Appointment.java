7509b3013cd1cbdf05ef1a11b75ba405e6f408c8
kvenkatasahil <125736172+kvenkatasahil@users.noreply.github.com>
2023-04-13 22:14:19 +0530

    Add files via upload

---
+++ Appointment.java
@@ -0,0 +1,54 @@
+import java.time.LocalTime;
+import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Appointment {
+    static ArrayList A1=new ArrayList ();
+    String Patient_name;
+    String Doctor_name;
+    String noofconsult;
+    String Appointment_no;
+    String Diseases;
+    LocalTime appoint_time;
+
+    public void add(String Patient_name,String Doctor_name,String noofconsult,String Appointment_no, String Diseases,LocalTime appoint_time)
+    {
+        this.Patient_name=Patient_name;
+        this.Doctor_name=Doctor_name;
+        this.noofconsult=noofconsult;
+        this.Appointment_no=Appointment_no;
+        this.Diseases=Diseases;
+        this.appoint_time=appoint_time;
+
+
+        A1.add(Patient_name);
+        A1.add(Doctor_name);
+        A1.add(noofconsult);
+        A1.add(Appointment_no);
+        A1.add(Diseases);
+        String x=appoint_time.toString();
+        A1.add(x);
+
+    }
+    public void Write()
+    {
+        try
+        {
+            FileWriter ob1=new FileWriter("Appointments.txt",true);
+            for(int i=0;i< A1.size();i+=6)
+            {
+
+                ob1.write(A1.get(i)+"\n"+A1.get(i+1)+"\n"+A1.get(i+2)+"\n"+A1.get(i+3)+"\n"+A1.get(i+4)+"\n"+A1.get(i+5)+"\n");
+            }
+            ob1.close();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- Project.java
+++ Project.java
@@ -1,12 +1,14 @@
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.*;
 public class Project {
 
 
-    public static void main(String[] args)
-    {
-        ArrayList D1= new ArrayList ();
+    public static void main(String[] args) throws IOException {
+        ArrayList  D1= new ArrayList ();
         ArrayList  P1= new ArrayList();
+        ArrayList  A1 = new ArrayList();
 
         int n;
         String name = null;
@@ -16,15 +18,24 @@ public class Project {
         String age=null;
         String ph_no = null;
         String  weight = null;
-        String Doc_relatedto;
+        String Doc_relatedto=null;
+        String Patient_name=null;
+        String Doctor_name=null;
+        String noofconsult=null;
+        String Appointment_no=null;
+        String Diseases=null;
+
 
         Scanner kvs=new Scanner(System.in);
         System.out.println("**************************************************");
         System.out.println("  ---------   WELCOME TO KVS HOSPITAL ------------");
         System.out.println("**************************************************");   
-        System.out.println("Enter the 1 for Patient data");
-        System.out.println("Enter the 2 for Appointment data");
-        System.out.println("Enter the 3 for Doctor data");
+        System.out.println("Enter the 1 to add Patient data");
+        System.out.println("Enter the 2 to Book Appointment ");
+        System.out.println("Enter the 3 to add Doctor data");
+        System.out.println("Enter the 4 for Patient data");
+        System.out.println("Enter the 5 for Doctor data");
+        System.out.println("Enter the 6 for Appointment data");
         n= kvs.nextInt();
 
         //Scanner kvs=new Scanner(System.in);
@@ -36,8 +47,12 @@ public class Project {
         
         switch (n){
             case 1: {
+                ReadPatient.read();
                 int control_input = 0;
                 Patient ob1 = new Patient();
+                int Patientidt=ReadPatient.Patients.size()-8;
+                String Patientidl=(String)ReadPatient.Patients.get(Patientidt);
+                int Patientid=Integer.parseInt(Patientidl);
                 while (control_input != 1) {
                     kvs.nextLine();
                     System.out.println("Enter Patient Name");
@@ -54,21 +69,47 @@ public class Project {
                     Doc_relatedto = kvs.nextLine();
                     LocalDate dateofjoin=LocalDate.now();
 
-                    ob1.add(name,gender,age, ph_no, weight, Doc_relatedto,dateofjoin);
-                    System.out.println(ob1);
+                    Patientid+=1;
+                    ob1.add(name,gender,age, ph_no, weight, Doc_relatedto,dateofjoin,Patientid);
                     System.out.println("Do you have  any further registrations : ");
-                    System.out.println("Enter 0 to add patient");
-                    System.out.println("Enter 1 not to add the patient");
+                    System.out.println("Enter 0 for add another patient");
+                    System.out.println("Enter 1 to complete");
                     control_input = kvs.nextInt();
                 }
                 ob1.Write();
                     break;
                 }
-
+            case 2:{
+                int control_input = 0;
+                Appointment ob1= new Appointment();
+                while (control_input!=1){
+                    kvs.nextLine();
+                    System.out.println("Enter Patient Name");
+                    Patient_name = kvs.nextLine();
+                    System.out.println("Enter Doctor Name");
+                    Doctor_name = kvs.nextLine();
+                    System.out.println("Enter the Disease");
+                    Diseases= kvs.nextLine();
+                    noofconsult="1";
+                    Appointment_no="1258463";
+                    LocalTime appoint_time= LocalTime.now();
+                    ob1.add(Patient_name,Doctor_name,noofconsult,Appointment_no, Diseases,appoint_time);
+                    System.out.println("Do you have  any further registrations : ");
+                    System.out.println("Enter 0 for add another patient");
+                    System.out.println("Enter 1 to complete");
+                    control_input = kvs.nextInt();
+                }
+                ob1.Write();
+                break;
+            }
 
             case 3:{
+                ReadDoctor.read();
                 int control_input = 0;
                 Doctor ob1 = new Doctor();
+                int Doctoridt=ReadDoctor.Doctors.size()-6;
+                String Doctoridl=(String)ReadDoctor.Doctors.get(Doctoridt);
+                int Doctorid=Integer.parseInt(Doctoridl);
                 while (control_input != 1) {
                     kvs.nextLine();
                     System.out.println("Enter Doctor Name");
@@ -81,10 +122,8 @@ public class Project {
                     ph_no = kvs.nextLine();
                     LocalDate dateofjoin=LocalDate.now();
 
-
-                    ob1.add(name,designation,specialization,ph_no,dateofjoin);
-
-
+                    Doctorid+=1;
+                    ob1.add(name,designation,specialization,ph_no,dateofjoin,Doctorid);
                     System.out.println("Enter 0 for add another doctor");
                     System.out.println("Enter 1 to complete");
                     control_input = kvs.nextInt();
@@ -92,13 +131,63 @@ public class Project {
                 ob1.Write();
                 break;
             }
+            case 4:
+            {
+                ReadPatient.read();
+                int x=ReadPatient.Patients.size();
+                for(int i=0;i<x;i+=8)
+                {
+                    System.out.println("Patient Id: "+ReadPatient.Patients.get(i));
+                    System.out.println("Patient Name: "+ReadPatient.Patients.get(i+1));
+                    System.out.println("Patient Gender: "+ReadPatient.Patients.get(i+2));
+                    System.out.println("Patient Age: "+ReadPatient.Patients.get(i+3));
+                    System.out.println("Patient Phone Number: "+ReadPatient.Patients.get(i+4));
+                    System.out.println("Patient Weight: "+ReadPatient.Patients.get(i+5));
+                    System.out.println("Patient's Doctor: "+ReadPatient.Patients.get(i+6));
+                    System.out.println("Patient's Admitted date: "+ReadPatient.Patients.get(i+7));
+                    System.out.println("----------------------------------------------------------");
+                }
+                break;
+            }
+            case 5:
+            {
+                ReadDoctor.read();
+                int x=ReadDoctor.Doctors.size();
+                for(int i=0;i<x;i+=6)
+                {
+                    System.out.println("Doctor Id: "+ReadDoctor.Doctors.get(i));
+                    System.out.println("Doctor Name: "+ReadDoctor.Doctors.get(i+1));
+                    System.out.println("Doctor Designation: "+ReadDoctor.Doctors.get(i+2));
+                    System.out.println("Doctor Specialization: "+ReadDoctor.Doctors.get(i+3));
+                    System.out.println("Doctor Phone Number: "+ReadDoctor.Doctors.get(i+4));
+                    System.out.println("Doctor Joined Date: "+ReadDoctor.Doctors.get(i+5));
+                    System.out.println("----------------------------------------------------------");
+
+                }
+                break;
+            }
+            case 6:
+            {
+                ReadAppointment.read();
+                int x=ReadAppointment.Apponitments.size();
+                for(int i=0;i<x;i+=6)
+                {
+                    System.out.println("Appointment No. :"+(i+1));
+                    System.out.println("Patient Name: "+ReadAppointment.Apponitments.get(i));
+                    System.out.println("Doctor Name: "+ReadAppointment.Apponitments.get(i+1));
+                    System.out.println("No of Consultations: "+ReadAppointment.Apponitments.get(i+2));
+                    System.out.println("Appointment Number: "+ReadAppointment.Apponitments.get(i+3));
+                    System.out.println("Disease: "+ReadAppointment.Apponitments.get(i+4));
+                    System.out.println("Appointment time: "+ReadAppointment.Apponitments.get(i+5));
+                    System.out.println("----------------------------------------------------------");
+
+                }
+                break;
+            }
+
             default:{
                 System.out.println("Wrong Value entered");
             }
-
-            
         }
-        
-
     }
 }
